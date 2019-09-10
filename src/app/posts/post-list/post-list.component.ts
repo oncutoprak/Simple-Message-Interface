7100e091ef3d9c45e94053b325a5fbc7d3b9b489
Toprak05 <oncutoprak05@gmail.com>
2019-09-10 12:30:46 +0300

    Fisrt send

---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit, OnDestroy } from "@angular/core";
+import {Subscription } from 'rxjs';
+
+import { Post } from '../post.model';
+import { PostsService } from '../posts.service';
+
+@Component({
+  selector: "app-post-list",
+  templateUrl: "./post-list.component.html",
+  styleUrls: ["./post-list.component.css"]
+})
+
+export class PostListComponent implements OnInit, OnDestroy {
+  /*posts = [
+    {title: "First Post ", content: "This is th first post\'s content"},
+    {title: "Second Post ", content: "This is th second post\'s content"},
+    {title: "Third Post ", content: "This is th third post\'s content"},
+  ];*/
+  posts: Post[] = [];
+
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsService) {}
+
+  ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener()
+    .subscribe((posts: Post[]) => {
+
+      this.posts=posts;
+
+    });
+  }
+
+  ngOnDestroy() {
+    this.postsSub.unsubscribe();
+
+  }
+}
